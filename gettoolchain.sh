adda49f601d22b489016a732d9d7ba2ee8a35b91
Ryan Harvey <Harvey.RyanK@gmail.com>
2016-02-28 23:05:33 -0700

    not even sure why I made this a branch

--- gettoolchain.sh
+++ gettoolchain.sh
@@ -1,3 +1,12 @@
+if [ "$1" = "clean" -o "$1" = "refresh" ]
+then
+	rm -rf toolchain
+	if [ "$1" = "clean" ]
+	then
+		exit
+	fi
+fi
 git clone https://github.com/Nibble-Knowledge/toolchain.git
 cd toolchain
+git checkout plr4
 ./update.sh
